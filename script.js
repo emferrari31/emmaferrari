b75756b4c703936bfda1af6c6cef1bd422476552
Emma Ferrari <e.ferrari@live.co.uk>
2024-10-21 14:52:09 +0100

    html structure and basic js animation started

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText",{
+    strings : ["Emma","a Full Stack Developer"," an iO Academy Graduate"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+})
